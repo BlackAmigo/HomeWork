c8ac7f1d213389a41a9aa4f27597e511bb85d909
BlackAmigo <amigoz87@mail.ru>
2019-05-25 15:37:15 +0700

    добавлена еще одна реализация TimeConversion

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         TimeConversion timeConversion = new TimeConversion();
         System.out.println(timeConversion.to24format("07:05:45PM"));
+        System.out.println(timeConversion.to24formatOther("07:05:45PM"));
 
         KangarooShow ks = new KangarooShow();
         System.out.println(ks.isKangarooCrossing(2,1,1,2));
--- src/TimeConversion.java
+++ src/TimeConversion.java
@@ -19,4 +19,26 @@ public class TimeConversion {
         if(inputTime != null) return new SimpleDateFormat("HH:mm:ss").format(inputTime);
         else return "";
     }
+
+    public String to24formatOther(String input){
+        String isNumber = input.substring(0,2) + input.substring(3,5) + input.substring(6,8);
+        String colon = input.substring(2,3) + input.substring(5,6);
+        String pm_am = input.substring(8,10);
+
+        try {
+             int number = Integer.parseInt(isNumber);
+        }catch (NumberFormatException nfe){
+            return "Неверный формат" + " \"" + input + "\"";
+        }
+
+        if((colon.equals("::")) && ((pm_am.equals("AM") || (pm_am.equals("PM"))))) {
+            if (pm_am.equals("PM")){
+                int our = Integer.parseInt(input.substring(0,2));
+                int res = our + 12;
+                return res + input.substring(2,8);
+            }else {
+                return input.substring(0,9);
+            }
+        }else return "Неверный формат" + " \"" + input + "\"";
+    }
 }
